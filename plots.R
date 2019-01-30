243c339a9b33758567b5c851f969754783100e23
Cristian Groza <cristian.groza@protonmail.com>
2019-01-29 19:54:19 -0500

    Added closed form values.

--- plots.R
+++ plots.R
@@ -3,7 +3,10 @@ library(ggplot2)
 
 gdweigths <- read_csv("weights.tsv", col_names = T) %>% gather(key = "Term", "Value")
 
+lsweights <- tibble(Term =c("W1","W2","W3","W4","W5","W6"), Value = c(-1.09098000e+00,  4.05789421e-01, -2.28999224e-01,  1.19484466e-03, -8.56546449e-04,  7.91201104e-01))
+
 weight.plot <- ggplot(gdweigths) + geom_boxplot(aes(x = Term, y = Value)) + geom_jitter(aes(x=Term, y=Value)) +
+    geom_point(data=lsweights, aes(x=Term, y=Value), color = "blue") +
     ggtitle("Gradient descent stability") + labs(x = "Linear regression coefficient", y = "Value at convergence")
 ggsave("WeightStability.pdf", weight.plot)
 
